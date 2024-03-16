4ca84e3e62c50be9ea2b683e3e0b8e5ca568f547
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2024-03-16 00:45:35 +0000

    Added thermal images

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const PORT = 2000;
 const socket = new WebSocket(`ws://${HOST}:${PORT}`);
 
 const DOM = {
+	imgContainer: document.querySelector(".image-container"),
 	uPRN: document.getElementById("uprn"),
 	longitude: document.getElementById("longitude"),
 	latitude: document.getElementById("latitude"),
@@ -13,6 +14,21 @@ const DOM = {
 	category: document.getElementById("category"),
 	yearBuilt: document.getElementById("year-built"),
 	ePCRating: document.getElementById("epc-rating"),
+};
+
+const createImgElement = (src) => {
+	const element = document.createElement("img");
+	element.classList.add("preview");
+	element.src = src;
+	element.setAttribute("alt", "Preview image");
+	return element;
+};
+
+const loadImages = (directoryName, extension = "png") => {
+	for (let i = 1; i < 10; i++) {
+		const imgElement = createImgElement(`img/${directoryName}/${directoryName}${i}.${extension}`);
+		DOM.imgContainer.appendChild(imgElement);
+	}
 };
 
 socket.addEventListener("open", (event) => {
@@ -52,3 +68,5 @@ socket.addEventListener("message", (event) => {
 		console.log(event.data);
 	}
 });
+
+loadImages("elgin", "jpg");
